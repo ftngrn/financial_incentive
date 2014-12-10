856a20a0b4715ca5d8f48565c6b62baae1bca33c
Noriaki Ishihara <sf.n.ishihara+github@gmail.com>
2014-12-10 10:16:22 +0900

    Add sources

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>		<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>		 <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>		 <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>就園奨励費のめやす - 学校法人 札幌学園</title>
+	</head>
+<body>
+
+<h1>就園奨励費のめやす</h1>
+
+<dl class="dl-horizontal">
+	<dt>年収を入れて下さい</dt>
+	<dd>約<input type="text" name="ai" id="ai" />万円</dd>
+	<dt></dt><dd><button class="btn btn-lg btn-primary" name="calc" id="calc">計算する</button></dd>
+</dl>
+
+<hr />
+
+<dl class="dl-horizontal">
+	<dt>推定所得割額</dt>
+	<dd><input type="text" name="pil" id="pil" readonly="readonly" />円</dd>
+	<dt>就園奨励費のめやす</dt>
+	<dd><input type="text" name="pil" id="pil" readonly="readonly" />円</dd>
+</dl>
+
+<script type="text/javascript">
+//<![CDATA[
+
+//]]>
+</script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="per_income_levy.js"></script>
+</body>
+</html>
